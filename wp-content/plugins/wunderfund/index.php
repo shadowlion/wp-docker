628dfb0656f5e6a7c33c486baf13afef5209e7d1
Justin Chiou <chiou.kai@gmail.com>
2020-07-30 13:22:00 -0400

    setup specific plugin and theme folder

---
+++ wp-content/plugins/wunderfund/index.php
@@ -0,0 +1,9 @@
+<?php
+/*
+  Plugin Name: Hello WordPress
+  Plugin URI: http://wordpress.org/plugins/hello-wordpress/
+  Description: Hello WordPress!
+  Author: John Doe
+  Version: 1.0.0
+  Author URI: http://example.org/
+*/
