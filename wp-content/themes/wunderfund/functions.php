d8901083ce1bbf33494ac393a6d1689d9b308aa7
Justin Chiou <chiou.kai@gmail.com>
2020-07-30 13:22:48 -0400

    change tabs to spaces

--- wp-content/themes/wunderfund/functions.php
+++ wp-content/themes/wunderfund/functions.php
@@ -2,38 +2,38 @@
 
 function theme_styles() {
 
-	wp_enqueue_style(
-		'bootstrap',
-		'//stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css',
-	);
+  wp_enqueue_style(
+    'bootstrap',
+    '//stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css',
+  );
 
-	wp_enqueue_script(
-		'jquery',
-		'//code.jquery.com/jquery-3.5.1.slim.min.js',
-		array(),
-		'',
-		true
-	);
+  wp_enqueue_script(
+    'jquery',
+    '//code.jquery.com/jquery-3.5.1.slim.min.js',
+    array(),
+    '',
+    true
+  );
 
-	wp_enqueue_script(
-		'popper',
-		'//cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js',
-		array(),
-		'',
-		true
-	);
+  wp_enqueue_script(
+    'popper',
+    '//cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js',
+    array(),
+    '',
+    true
+  );
 
-	wp_enqueue_script(
-		'bootstrapjs',
-		'//stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js',
-		array(),
-		'',
-		true
-	);
+  wp_enqueue_script(
+    'bootstrapjs',
+    '//stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js',
+    array(),
+    '',
+    true
+  );
 
-	// Any custom CSS
-	wp_enqueue_style('style', get_stylesheet_uri(), array(), '0.1.0');
-	// wp_enqueue_script('script', )
+  // Any custom CSS
+  wp_enqueue_style('style', get_stylesheet_uri(), array(), '0.1.0');
+  // wp_enqueue_script('script', )
 
 }
 
